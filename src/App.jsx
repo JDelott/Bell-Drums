c264aab4537097906bdc88535a090b8b12b58af3
Jacob Delott <jldelott@gmail.com>
2024-01-22 21:30:34 -0800

    installed vite and setup navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navbar from "./NavBar";
+
+function App() {
+  
+
+  return (
+  <div>
+    <Navbar />
+  </div>
+  )
+}
+
+export default App
---
+++ src/NavBar.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+import './NavBar.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+
+
+
+
+const Navbar = () => {
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+
+    return (
+        <div className='header'>
+            <div className='container'>
+                <h1>Bell<span className='primary'>Drums</span></h1>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>
+                        <a href='/'>Home</a>
+                    </li>
+                    <li>
+                        <a href='/'>Drums</a>
+                    </li>
+                    <li>
+                        <a href='/'>About</a>
+                    </li>
+                    <li>
+                        <a href='/'>Contact</a>
+                    </li>
+                </ul>
+                <div className='btn-group'>
+                    <button className='btn'>Login/Signup</button>
+                </div>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{color: '#333'}}/>) : (<FaBars size={20} style={{color: '#333'}} />)}
+                    
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
