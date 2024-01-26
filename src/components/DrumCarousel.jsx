b50777d92754be2464617f40f6559ac2de6f24fe
Jacob Delott <jldelott@gmail.com>
2024-01-26 00:02:34 -0800

    added carousel

--- src/components/DrumCarousel.jsx
+++ src/components/DrumCarousel.jsx
@@ -17,18 +17,19 @@ function DrumCarousel() {
 
   return (
     <div className="carousel-container">
-     <label className="carousel-label">Bell Drum 1</label>
+     {/* <label className="carousel-label">Bell Drum 1</label> */}
       <div id={`slide${currentSlide}`} className="carousel-item">
         <div className="carousel-image-container">
           <img src={currentSlide === 1 ? image1 : currentSlide === 2 ? image2 : image3} className="carousel-image" alt={`Slide ${currentSlide}`} />
         </div>
-        <div className="carousel-line"></div>
         <div className="carousel-info">
          
-          <label className="carousel-label">Price:</label>
-          <span className="carousel-price">$10.00</span>
+          
           <label className="carousel-label">Description:</label>
           <p className="carousel-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <label className="carousel-label">Price:</label>
+          <span className="carousel-price">$10.00</span>
+
         </div>
         <button onClick={prevSlide} className="carousel-button carousel-button-prev">❮</button>
         <button onClick={nextSlide} className="carousel-button carousel-button-next">❯</button>
